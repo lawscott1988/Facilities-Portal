d4eb38decba624f75b460bf9889b770baace2a6d
Scott Law <slaw@futurecities.catapult.org.uk>
2019-03-14 12:58:58 +0000

    added filter on click function

---
+++ js/common.js
@@ -0,0 +1,9 @@
+function dropDownFunc() {
+	var x = document.getElementById("filterDropDown");
+	if(x.style.display == "none") {
+		x.style.display = "block";
+	}
+	else {
+		x.style.display = "none";
+	}
+}
