73aeb4a10a9c30f20481228726086b46377d04d7
lawscott1988 <lawscott1988@googlemail.com>
2019-03-19 21:22:49 +0000

    added modal popup onclick of new key box button

--- js/common.js
+++ js/common.js
@@ -1,3 +1,5 @@
+//FILTER DROPDOWN MENU
+
 function dropDownFunc() {
 	var x = document.getElementById("filterDropDown");
 	if(x.style.display == "none") {
@@ -6,4 +8,16 @@ function dropDownFunc() {
 	else {
 		x.style.display = "none";
 	}
+}
+
+//MODAL BOXES FOR BUTTONS
+
+function displayModal(elt) {
+	var x = document.getElementById(elt);
+	x.style.display = "block";
+}
+
+function closeModal() {
+	var x = document.getElementById("newKeyBoxModal");
+	x.style.display = "none";
 }
