2fccd4001e29bc8f9e42afe162d1077f3d9de935
lawscott1988 <lawscott1988@googlemail.com>
2019-04-08 15:31:55 +0100

    added functionality to dropdown burger icon; attempting to fix burger icon on click. it is not working yet

--- js/common.js
+++ js/common.js
@@ -21,3 +21,35 @@ function closeModal(elt) {
 	var x = document.getElementById(elt);
 	x.style.display = "none";
 }
+
+function expandMenu() {
+
+	var burger = document.getElementById("burgerIcon");
+
+	if(burger.className === "icon") {
+		burger.className = "icon_clicked";
+	}
+	else {
+		burger.className = "icon";
+	}
+
+	var link1 = document.getElementById("facInfoL");
+	var link2 = document.getElementById("logActionL");
+	var link3 = document.getElementById("viewActionL");
+	var link4 = document.getElementById("keySystemL");
+
+	var links = [
+		link1,
+		link2,
+		link3,
+		link4,
+	]
+	for (i = 0; i < links.length; i++) {
+		if(links[i].style.display == "none") {
+			links[i].style.display = "block";
+		}
+		else {
+			links[i].style.display = "none";
+		}
+	}
+}
